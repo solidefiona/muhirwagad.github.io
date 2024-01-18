b6ef4c5e91f79e4e39b785b64662e51668c1b3c7
solidefiona <78968817+solidefiona@users.noreply.github.com>
2024-01-18 19:34:17 +0200

    Add files via upload

---
+++ Wireframe.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x, y, z = axes3d.get_test_data()
+
+ax.plot_wireframe(x, y, z, rstride=2, cstride=2)
+
+plt.title("Wireframe Plot Example")
+plt.tight_layout()
+plt.show()
